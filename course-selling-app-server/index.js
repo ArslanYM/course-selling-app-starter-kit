4657697b661439f357bb017f0bafe94bb10a38a8
ArslanYM <arsalanyaqmalik@gmail.com>
2023-09-17 19:57:18 +0530

    added recoil and mongoDB

--- course-selling-app-server/index.js
+++ course-selling-app-server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
@@ -9,12 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
-
-// Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
-
-app.listen(3000, () => console.log('Server running on port 3000'));
+mongoose
+  .connect(
+    "mongodb+srv://arsalanmalik:663663@cluster0.z2buagq.mongodb.net/courses",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "courses",
+    }
+  )
+  .then(() => {
+    app.listen(3000, () => console.log("Server running on port 3000"));
+  })
+  .catch((e) => {
+    console.error(e);
+  });
